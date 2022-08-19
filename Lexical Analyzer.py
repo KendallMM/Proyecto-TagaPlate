d791c6a1f81126a8dbded5a94b369432a1ab8411
KendallMM <kendallmarinmunoz@gmail.com>
2022-08-18 20:28:24 -0600

    Se añadieron las expresiones regulares necesarias para reconocer tokens.

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -4,35 +4,53 @@ import codecs
 import os
 import sys
 
-tokens = ["ID", "NUMBER", "ADD", "SUB", "MUL", "DIV", "GT", "LT", "GET", "LET",
-          "EQUAL", "DT", "FUNC", "COMMA", "DOT", "LPARENT", "RPARENT", "COMMENT"]
+tokens = ['ID', 'NUMBER', 'ADD', 'SUB', 'MUL', 'DIV', 'GT', 'LT', 'GET', 'LET',
+          'EQUAL', 'DT', 'FUNC', 'COMMA', 'DOT', 'LPARENT', 'RPARENT', 'COMMENT']
 
-keywords = {"New": "NEW",
-            "CALL": "CALL",
-            "Proc": "PROCEDURE",
-            "@Principal": "PRIN",
-            "True": "TRUE",
-            "False": "FALSE",
-            "Num": "NUMVAL",
-            "Bool": "BOOLVAL",
-            "Values": "VALUES",
-            "Alter": "ALTERA",
-            "AlterB": "ALTERB",
-            "MoveRight": "MOVR",
-            "MoveLeft": "MOVL",
-            "Hammer": "HAMM",
-            "Stop": "STOP",
-            "isTrue": "VERT",
-            "Repeat": "REPEAT",
-            "Until": "UNTIL",
-            "While": "WHILE",
-            "Case": "CASE",
-            "When": "WHEN",
-            "Then": "THEN",
-            "Else": "ELSE",
-            "PrintValues": "PRINT"}
+keywords = {'New': 'NEW',
+            'CALL': 'CALL',
+            'Proc': 'PROCEDURE',
+            '@Principal': 'PRIN',
+            'True': 'TRUE',
+            'False': 'FALSE',
+            'Num': 'NUMVAL',
+            'Bool': 'BOOLVAL',
+            'Values': 'VALUES',
+            'Alter': 'ALTERA',
+            'AlterB': 'ALTERB',
+            'MoveRight': 'MOVR',
+            'MoveLeft': 'MOVL',
+            'Hammer': 'HAMM',
+            'Stop': 'STOP',
+            'isTrue': 'VERT',
+            'Repeat': 'REPEAT',
+            'Until': 'UNTIL',
+            'While': 'WHILE',
+            'Case': 'CASE',
+            'When': 'WHEN',
+            'Then': 'THEN',
+            'Else': 'ELSE',
+            'PrintValues': 'PRINT'}
 
 tokens = tokens + list(keywords.values())
+
+t_ignore = '\t'
+t_SUMA = 'ADD'
+t_RESTA = 'SUB'
+t_MULTIPLICACION = 'MUL'
+t_DIVISION = 'DIV'
+t_MAYOR = '>'
+t_MENOR = '<'
+t_IGUAL = '=='
+t_DISTINTO = '<>'
+t_MENIGUAL = '<='
+t_MAYIGUAL = '>='
+
+def t_ID(t):
+    r'@[a-zA-Z_][a-zA-Z0-9_]*'
+def t_COMENTARIOS(t):
+    r'\--.*'
+    pass
 
 
 
