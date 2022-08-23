a92288a213c1af3ccedd43872d1e5d63c294e6a5
eduardo-bm10 <ebolivarminguet@gmail.com>
2022-08-23 00:30:11 -0600

    Modificar analizador

--- lexicalscanner.py
+++ lexicalscanner.py
@@ -4,8 +4,8 @@ import codecs
 import os
 import sys
 
-tokens = ['ID', 'NUMBER', 'ADD', 'SUB', 'MUL', 'DIV', 'GT', 'LT', 'GET', 'LET',
-          'EQUAL', 'DT', 'FUNC', 'COMMA', 'DOT', 'LPARENT', 'RPARENT', 'COMMENT']
+tokens = ['ID', 'NUMBER', 'ADD', 'SUB', 'MUL', 'DIV', 'MAYOR', 'MENOR', 'MAYIGUAL', 'MENIGUAL',
+          'IGUAL', 'DIS', 'FUNC', 'COMMA', 'DOT', 'LPARENT', 'RPARENT', 'COMMENT']
 
 keywords = {'New': 'NEW',
             'CALL': 'CALL',
@@ -34,27 +34,41 @@ keywords = {'New': 'NEW',
 
 tokens = tokens + list(keywords.values())
 
-t_ignore = '\t'
-t_SUMA = 'ADD'
-t_RESTA = 'SUB'
-t_MULTIPLICACION = 'MUL'
-t_DIVISION = 'DIV'
-t_MAYOR = '>'
-t_MENOR = '<'
-t_IGUAL = '=='
-t_DISTINTO = '<>'
-t_MENIGUAL = '<='
-t_MAYIGUAL = '>='
-t_LPARENT = '\('
-t_RPARENT = '\)'
-t_COMMA = ','
-t_DOT = '\.'
-def t_ID(t):
-    r'@[a-zA-Z_#][a-zA-Z0-9_#]*'
-def t_COMMENT(t):
-    r'\--.*'
-    pass
+t_ignore = r'\\t'
+t_ID = r'@[a-zA-Z0-9]{2,9}'
+t_NUMBER = r'[0-9]+'
+t_ADD = r'ADD'
+t_SUB = r'SUB'
+t_MUL = r'MUL'
+t_DIV = r'DIV'
+t_MAYOR = r'>'
+t_MENOR = r'<'
+t_IGUAL = r'=='
+t_DIS = r'<>'
+t_MENIGUAL = r'<='
+t_MAYIGUAL = r'>='
+t_LPARENT = r'\('
+t_RPARENT = r'\)'
+t_COMMA = r','
+t_DOT = r'\.'
+t_COMMENT = r'\--.*'
 
+def file_reader(path):
+    file = open(path, mode='r')
+    txt = file.read()
+    file.close()
+    return txt
+
+def scanner(txt):
+    x = re.findall(t_ID, txt)
+    if x:
+        return x
+    else:
+        return error_generator('variable')
+
+def error_generator(type):
+    return 'Lexical error: ' + type
+    
 
 
 
