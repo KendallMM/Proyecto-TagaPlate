9568601d1d12394ecaad9552671c3f64718f8f67
eduardo-bm10 <ebolivarminguet@gmail.com>
2022-09-13 21:33:50 -0600

    Changes by kendall

--- syntaxAnalyzer.py
+++ syntaxAnalyzer.py
@@ -125,101 +125,123 @@ def p_value4(p):
 
 def p_operator1(p):
     '''operator : ADD'''
-    #p[0] = Operator1(p[1], 'Operator1')
+    p[0] = Operator1(p[1], 'Operator1')
+
 def p_operator2(p):
     '''operator : SUB'''
-    #p[0] = Operator2(p[1], 'Operator2')
+    p[0] = Operator2(p[1], 'Operator2')
+
 def p_operator3(p):
     '''operator : MUL'''
-    #p[0] = Operator3(p[1], 'Operator3')
+    p[0] = Operator3(p[1], 'Operator3')
+
 def p_operator4(p):
     '''operator : DIV'''
-    #p[0] = Operator4(p[1], 'Operator4')
+    p[0] = Operator4(p[1], 'Operator4')
+
 def p_comparator1(p):
     '''comparator : GT'''
-    #p[0] = Comparator1(p[1], 'Comparator1')
+    p[0] = Comparator1(p[1], 'Comparator1')
+
 def p_comparator2(p):
     '''comparator : LT'''
-    #p[0] = Comparator2(p[1], 'Comparator2')
+    p[0] = Comparator2(p[1], 'Comparator2')
+
 def p_comparator3(p):
     '''comparator : GET'''
-    #p[0] = Comparator3(p[1], 'Comparator3')
+    p[0] = Comparator3(p[1], 'Comparator3')
+
 def p_comparator4(p):
     '''comparator : LET'''
-    #p[0] = Comparator4(p[1], 'Comparator4')
+    p[0] = Comparator4(p[1], 'Comparator4')
+
 def p_comparator5(p):
     '''comparator : EQ'''
-    #p[0] = Comparator5(p[1], 'Comparator5')
+    p[0] = Comparator5(p[1], 'Comparator5')
+
 def p_comparator6(p):
     '''comparator : DT'''
-    #p[0] = Comparator6(p[1], 'Comparator6')
+    p[0] = Comparator6(p[1], 'Comparator6')
 def p_position1(p):
     '''position : NORTH'''
-    #p[0] = Position1(p[1], 'Position1')
+    p[0] = Position1(p[1], 'Position1')
+
+
 def p_position2(p):
     '''position : SOUTH'''
-    #p[0] = Position2(p[1], 'Position2')
+    p[0] = Position2(p[1], 'Position2')
+
 def p_position3(p):
     '''position : EAST'''
-    #p[0] = Position3(p[1], 'Position3')
+    p[0] = Position3(p[1], 'Position3')
+
 def p_position4(p):
     '''position : WEST'''
-    #p[0] = Position4(p[1], 'Position4')
+    p[0] = Position4(p[1], 'Position4')
+
 def p_repeatInstructions(p):
     '''repeat_instructions : instructions BREAK'''
-    #p[0] = RepeatInstructions(p[1], p[2], 'RepeatInstructions')
+    p[0] = RepeatInstructions(p[1], p[2], 'RepeatInstructions')
+
 def p_untilbody(p):
     '''untilbody : UNTIL LPARENT instructions RPARENT condition'''
-    #p[0] = UntilBody(p[1], p[2],p[3], p[4],p[5], 'UntilBody')
+    p[0] = UntilBody(p[1], p[2],p[3], p[4],p[5], 'UntilBody')
+
 def p_whilebody(p):
     '''whilebody : WHILE condition LPARENT instructions RPARENT'''
-    #p[0] = WhileBody(p[1], p[2],p[3], p[4],p[5], 'WhileBody')
+    p[0] = WhileBody(p[1], p[2],p[3], p[4],p[5], 'WhileBody')
+
 def p_casebody1(p):
     '''casebody : CASE WHEN LPARENT condition RPARENT THEN LPARENT instructions RPARENT'''
-    #p[0] = CaseBody1(p[1],p[2],p[3], p[4],p[5],p[6],p[7],p[8],p[9], 'CaseBody1')
+    p[0] = CaseBody1(p[1],p[2],p[3], p[4],p[5],p[6],p[7],p[8],p[9], 'CaseBody1')
+
 def p_casebody2(p):
     '''casebody : casebody ELSE LPARENT instructions RPARENT'''
-    #p[0] = CaseBody2(p[1],p[2],p[3], p[4],p[5], 'CaseBody2')
+    p[0] = CaseBody2(p[1],p[2],p[3], p[4],p[5], 'CaseBody2')
+
 def p_casebody3(p):
     '''casebody : CASE ID innercasebody'''
-    #p[0] = CaseBody3(p[1],p[2],p[3], 'CaseBody3')
+    p[0] = CaseBody3(p[1],p[2],p[3], 'CaseBody3')
+
 def p_innercasebody1(p):
     '''innercasebody : WHEN NUMBER THEN LPARENT instructions RPARENT innercasebody'''
-    #p[0] = InnerCaseBody1(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody1')
+    p[0] = InnerCaseBody1(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody1')
+
 def p_innercasebody2(p):
     '''innercasebody : WHEN TRUE THEN LPARENT instructions RPARENT innercasebody'''
-    #p[0] = InnerCaseBody2(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody2')
+    p[0] = InnerCaseBody2(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody2')
+
 def p_innercasebody3(p):
     '''innercasebody : WHEN FALSE THEN LPARENT instructions RPARENT innercasebody'''
-    #p[0] = InnerCaseBody3(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody3')
+    p[0] = InnerCaseBody3(p[1],p[2],p[3], p[4],p[5],p[6],p[7], 'InnerCaseBody3')
+
 def p_innercasebodyEmpty(p):
     '''innercasebody : empty'''
-    print("nulo")
-    #p[0] = InnerCaseBodyEmpty(p[1], 'InnerCaseBodyEmpty')
+
 def p_condition1(p):
     '''condition : ID comparator ID'''
-    #p[0] = Condition1(p[1],p[2],p[3], 'Condition1')
+    p[0] = Condition1(p[1],p[2],p[3], 'Condition1')
 def p_condition2(p):
     '''condition : ID comparator NUMBER'''
-    #p[0] = Condition2(p[1],p[2],p[3], 'Condition2')
+    p[0] = Condition2(p[1],p[2],p[3], 'Condition2')
 def p_condition3(p):
     '''condition : NUMBER comparator ID'''
-    #p[0] = Condition3(p[1],p[2],p[3], 'Condition3')
+    p[0] = Condition3(p[1],p[2],p[3], 'Condition3')
 def p_condition4(p):
     '''condition : NUMBER comparator NUMBER'''
-    #p[0] = Condition4(p[1],p[2],p[3], 'Condition4')
+    p[0] = Condition4(p[1],p[2],p[3], 'Condition4')
 def p_condition5(p):
     '''condition : TRUE'''
-    #p[0] = Condition5(p[1], 'Condition5')
+    p[0] = Condition5(p[1], 'Condition5')
 def p_condition6(p):
     '''condition : FALSE'''
-    #p[0] = Condition6(p[1], 'Condition6')
+    p[0] = Condition6(p[1], 'Condition6')
 def p_condition7(p):
     '''condition : istrue'''
-    #p[0] = Condition7(p[1], 'Condition7')
+    p[0] = Condition7(p[1], 'Condition7')
 def p_istrue(p):
     '''istrue : VERT LPARENT ID RPARENT'''
-    #p[0] = IsTrue(p[1],p[2],p[3],p[4], 'IsTrue')
+    p[0] = IsTrue(p[1],p[2],p[3],p[4], 'IsTrue')
 def p_printstart(p):
     '''printstart : startvalue printvalues'''
     p[0] = PrintStart(p[1],p[2], 'PrintStart')
@@ -251,7 +273,7 @@ def p_empty(p):
 def p_error(p):
     global err
     global errorCounter
-    errorCounter+1
+    errorCounter = errorCounter + 2
     if p:
         if p.type != 'COMMENT' and p.lineno == 1:
             err = "Sintax error on line 1: Missing expected initial comment."
@@ -262,11 +284,6 @@ def sintax_analisis(path):
     global errorCounter
     parser = yacc.yacc()
     result = parser.parse(open(path).read())
-    #result.imprimir(" ")
-    #print(result.traducir())
-    #graphFile = open('graphtree','w')
-    #graphFile.write(result.traducir())
-    #graphFile.close()
     print(result)
     if (errorCounter==0):
         errorCounter=-1
